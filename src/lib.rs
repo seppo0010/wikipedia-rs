7808902bb0e2b1e58a507383ffd00f4600f1bbbe
Sebastian Waisbrot <seppo0010@gmail.com>
2015-09-19 10:33:12 -0700

    Add search

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,47 @@
 extern crate hyper;
 extern crate url;
+extern crate serde_json;
 
 use std::result;
+use std::io;
+use std::io::Read;
 
-use hyper::Url;
+use hyper::{Client, Url};
+use hyper::header::UserAgent;
 
 const LANGUAGE_URL_MARKER:&'static str = "{language}";
 
 #[derive(Debug)]
 pub enum Error {
     UrlError(url::ParseError),
+    HyperError(hyper::error::Error),
+    HTTPError(hyper::client::response::Response),
+    IOError(io::Error),
+    JSONError(serde_json::error::Error),
+    JSONPathError,
 }
 
 impl std::convert::From<url::ParseError> for Error {
     fn from(e: url::ParseError) -> Self {
         Error::UrlError(e)
+    }
+}
+
+impl std::convert::From<hyper::error::Error> for Error {
+    fn from(e: hyper::error::Error) -> Self {
+        Error::HyperError(e)
+    }
+}
+
+impl std::convert::From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Error::IOError(e)
+    }
+}
+
+impl std::convert::From<serde_json::error::Error> for Error {
+    fn from(e: serde_json::error::Error) -> Self {
+        Error::JSONError(e)
     }
 }
 
@@ -31,7 +58,7 @@ pub struct Wikipedia {
 impl Default for Wikipedia {
     fn default() -> Self {
         Wikipedia {
-            pre_language_url: "http://".to_owned(),
+            pre_language_url: "https://".to_owned(),
             post_language_url: ".wikipedia.org/w/api.php".to_owned(),
             user_agent: "wikipedia (https://github.com/seppo0010/wikipedia-rs)".to_owned(),
             language: "en".to_owned(),
@@ -66,30 +93,71 @@ impl Wikipedia {
                 ("list", "search"),
                 ("srprop", ""),
                 ("srlimit", results),
-                ("limit", results),
                 ("srsearch", query),
+                ("continue", ""),
+                ("format", "json"),
+                ("action", "query"),
                 ].into_iter());
         Ok(url)
+    }
+
+    pub fn search(&self, query: &str) -> Result<Vec<String>> {
+        let url = try!(self.search_url(query));
+        let client = Client::new();
+        let mut response = try!(client.get(url)
+            .header(UserAgent(self.user_agent.clone()))
+            .send());
+
+        if !response.status.is_success() {
+            return Err(Error::HTTPError(response));
+        }
+
+        let mut response_str = String::new();
+        try!(response.read_to_string(&mut response_str));
+
+        let data:serde_json::Value = try!(serde_json::from_str(&*response_str));
+
+        // There has to be a better way to write the following code
+        Ok(try!(
+            data.as_object()
+            .and_then(|x| x.get("query"))
+            .and_then(|x| x.as_object())
+            .and_then(|x| x.get("search"))
+            .and_then(|x| x.as_array())
+            .ok_or(Error::JSONPathError)
+            ).into_iter().filter_map(|i|
+                i.as_object()
+                .and_then(|i| i.get("title"))
+                .and_then(|s| s.as_string().map(|s| s.to_owned()))
+                ).collect())
     }
 }
 
 #[test]
 fn base_url() {
     let mut wikipedia = Wikipedia::default();
-    assert_eq!(wikipedia.base_url(), "http://en.wikipedia.org/w/api.php");
+    assert_eq!(wikipedia.base_url(), "https://en.wikipedia.org/w/api.php");
     wikipedia.language = "es".to_owned();
-    assert_eq!(wikipedia.base_url(), "http://es.wikipedia.org/w/api.php");
+    assert_eq!(wikipedia.base_url(), "https://es.wikipedia.org/w/api.php");
 
-    wikipedia.set_base_url("http://hello.{language}.world/");
-    assert_eq!(wikipedia.base_url(), "http://hello.es.world/");
+    wikipedia.set_base_url("https://hello.{language}.world/");
+    assert_eq!(wikipedia.base_url(), "https://hello.es.world/");
 
-    wikipedia.set_base_url("http://hello.world/");
-    assert_eq!(wikipedia.base_url(), "http://hello.world/");
+    wikipedia.set_base_url("https://hello.world/");
+    assert_eq!(wikipedia.base_url(), "https://hello.world/");
 }
 
 #[test]
 fn search_url() {
     let wikipedia = Wikipedia::default();
     assert_eq!(&*format!("{}", wikipedia.search_url("hello world").unwrap()),
-            "http://en.wikipedia.org/w/api.php?list=search&srprop=&srlimit=10&limit=10&srsearch=hello+world");
+            "https://en.wikipedia.org/w/api.php?list=search&srprop=&srlimit=10&srsearch=hello+world&continue=&format=json&action=query");
+}
+
+#[test]
+fn search() {
+    let wikipedia = Wikipedia::default();
+    let results = wikipedia.search("hello world").unwrap();
+    assert!(results.len() > 0);
+    assert!(results.contains(&"\"Hello, World!\" program".to_owned()));
 }
