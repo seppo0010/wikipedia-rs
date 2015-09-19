4f7cc0e97cc39e47507702b1037e2da62ceda585
Sebastian Waisbrot <seppo0010@gmail.com>
2015-09-19 09:29:53 -0700

    Add search_url

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,31 @@
+extern crate hyper;
+extern crate url;
+
+use std::result;
+
+use hyper::Url;
+
 const LANGUAGE_URL_MARKER:&'static str = "{language}";
+
+#[derive(Debug)]
+pub enum Error {
+    UrlError(url::ParseError),
+}
+
+impl std::convert::From<url::ParseError> for Error {
+    fn from(e: url::ParseError) -> Self {
+        Error::UrlError(e)
+    }
+}
+
+pub type Result<T> = result::Result<T, Error>;
 
 pub struct Wikipedia {
     pub pre_language_url:String,
     pub post_language_url:String,
     pub user_agent:String,
     pub language:String,
+    pub search_results:u32,
 }
 
 impl Default for Wikipedia {
@@ -14,6 +35,7 @@ impl Default for Wikipedia {
             post_language_url: ".wikipedia.org/w/api.php".to_owned(),
             user_agent: "wikipedia (https://github.com/seppo0010/wikipedia-rs)".to_owned(),
             language: "en".to_owned(),
+            search_results: 10,
         }
     }
 }
@@ -36,6 +58,19 @@ impl Wikipedia {
         self.pre_language_url = base_url[0..index].to_owned();
         self.post_language_url = base_url[index+LANGUAGE_URL_MARKER.len()..].to_owned();
     }
+
+    fn search_url(&self, query: &str) -> Result<Url> {
+        let mut url = try!(Url::parse(&*self.base_url()));
+        let results = &*format!("{}", self.search_results);
+        url.set_query_from_pairs(vec![
+                ("list", "search"),
+                ("srprop", ""),
+                ("srlimit", results),
+                ("limit", results),
+                ("srsearch", query),
+                ].into_iter());
+        Ok(url)
+    }
 }
 
 #[test]
@@ -50,4 +85,11 @@ fn base_url() {
 
     wikipedia.set_base_url("http://hello.world/");
     assert_eq!(wikipedia.base_url(), "http://hello.world/");
+}
+
+#[test]
+fn search_url() {
+    let wikipedia = Wikipedia::default();
+    assert_eq!(&*format!("{}", wikipedia.search_url("hello world").unwrap()),
+            "http://en.wikipedia.org/w/api.php?list=search&srprop=&srlimit=10&limit=10&srsearch=hello+world");
 }
